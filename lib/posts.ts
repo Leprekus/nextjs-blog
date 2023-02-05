1865a7a6bf4607dd53a8799279bb0e41374f3f09
Leprekus <leprekus@yahoo.com>
2023-02-05 13:07:59 -0600

    render blog content

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,10 +1,15 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
+import { remark } from 'remark';
+import html from 'remark-html';
 
 const postsDirectory: string = path.join(process.cwd(), 'posts')
-
-export function getSortedPostsData() {
+interface postObject {
+    id: string,
+    date: string
+}
+export function getSortedPostsData():object {
     //gets file names under /posts
     const fileNames: string[] = fs.readdirSync(postsDirectory);
     const allPostsData = fileNames.map(fileName => {
@@ -23,10 +28,56 @@ export function getSortedPostsData() {
             ...matterResult.data
         }
     })
-    return allPostsData.sort((a, b) => {
+    return allPostsData.sort((a:postObject, b: postObject) => {
         if(a.date < b.date) {
             return 1;
         }
         return -1;
     })
 }
+
+export function getAllPostIds() {
+    const fileNames = fs.readdirSync(postsDirectory);
+  
+    // Returns an array that looks like this:
+    // [
+    //   {
+    //     params: {
+    //       id: 'ssg-ssr'
+    //     }
+    //   },
+    //   {
+    //     params: {
+    //       id: 'pre-rendering'
+    //     }
+    //   }
+    // ]
+    return fileNames.map((fileName) => {
+      return {
+        params: {
+          id: fileName.replace(/\.md$/, ''),
+        },
+      };
+    });
+  }
+
+  export async function getPostData(id: string) {
+    const fullPath = path.join(postsDirectory, `${id}.md`);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+  
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
+  
+    // Use remark to convert markdown into HTML string
+    const processedContent = await remark()
+      .use(html)
+      .process(matterResult.content);
+    const contentHtml = processedContent.toString();
+  
+    // Combine the data with the id and contentHtml
+    return {
+      id,
+      contentHtml,
+      ...matterResult.data,
+    };
+  }
