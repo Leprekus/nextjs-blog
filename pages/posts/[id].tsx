6e392d7141648bb3593d51973daec40892693fd8
Leprekus <leprekus@yahoo.com>
2023-02-05 14:29:01 -0600

    uncomment [id]

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -21,18 +21,17 @@ export async function getStaticPaths() {
 }
 export default function Post({ postData }) {
     return (
-    //   <Layout home=''>
-    //     <Head>
-    //       <title>{postData.title}</title>
-    //     </Head>
-    //     <article>
-    //       <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-    //       <div className={utilStyles.lightText}>
-    //         <Date dateString={postData.date} />
-    //       </div>
-    //       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-    //     </article>
-    //   </Layout>
-    null
+      <Layout home=''>
+        <Head>
+          <title>{postData.title}</title>
+        </Head>
+        <article>
+          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+          <div className={utilStyles.lightText}>
+            <Date dateString={postData.date} />
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
+      </Layout>
     );
   }
